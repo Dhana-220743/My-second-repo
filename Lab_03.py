dfc969529fdf9d4db753bda575c109e7ddc74f10
Dhana-220743 <n220743@college.edu>
2026-03-01 14:53:20 +0530

    Regular expressions lab is completed

--- Lab_03.py
+++
@@ -1,8 +0,0 @@
-import re
-
-''' pattern= r'(\d\d\d)-(\d\d\d-\d\d\d\d)'
-text = "Helllloooo 938-107-2590"
-match=re.search(pattern,text)
-print(match.group(0))
-print(match.group(1))
-print(match.group(2)) '''
